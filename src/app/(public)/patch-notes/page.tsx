a2a4d1ee9a0932b9abac2a82a4948961e81b29e3
Felipe <feliperenanrf23@gmail.com>
2024-10-10 11:36:19 -0300

    ajuste erros de build

--- src/app/(public)/patch-notes/page.tsx
+++ src/app/(public)/patch-notes/page.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { PropositionsList } from './components/propositions.list';
 import { ScrollText } from 'lucide-react';
 import { DiagonalBackground } from '@/components/ui/diagonal-background';
@@ -7,11 +7,13 @@ const PatchNotes = () => {
   return (
     <div className="h-full w-full overflow-hidden bg-main-bg">
       <DiagonalBackground>
-        <div className="flex w-full p-4 pb-1 gap-4 items-center">
-          <ScrollText size={50} />
-          <h1 className="text-4xl font-bold">Patch Notes</h1>
-        </div>
-        <PropositionsList></PropositionsList>
+        <Suspense fallback={<div>Loading...</div>}>
+          <div className="flex w-full p-4 pb-1 gap-4 items-center">
+            <ScrollText size={50} />
+            <h1 className="text-4xl font-bold">Patch Notes</h1>
+          </div>
+          <PropositionsList></PropositionsList>
+        </Suspense>
       </DiagonalBackground>
     </div>
   );
